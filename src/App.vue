56afd35a3cd0925269a272785faca612ab6dccce
Jan Madeyski <jan.madeyski@gmail.com>
2024-04-27 10:57:54 +0200

    fix bg

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@ import MainPage from '@/pages/MainPage.vue';
 <style>
 #app {
   background: radial-gradient(circle at 0% 90%, #0afa 0%, #0af0 35%),
-    radial-gradient(circle at 90% 0%, #aafa 0%, #aaf0 45%),
-    radial-gradient(circle at 0% 10%, #fff0 20%, #fff4 22%, #fff0 24%),
-    radial-gradient(circle at 100% 90%, #fff0 30%, #fff4 34%, #fff0 38%);
+    radial-gradient(circle at 190vh 100vh, #aafa 0%, #aaf0 45%),
+    radial-gradient(circle at 0% 110vh, #fff0 20%, #fff4 22%, #fff0 24%),
+    radial-gradient(circle at 200vh 190vh, #fff0 30%, #fff4 34%, #fff0 38%);
   background-color: #023;
 }
 
