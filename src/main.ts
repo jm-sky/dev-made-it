beadd1c7a8e30ca4d023a2a19701ca49fce361a9
Jan Madeyski <jan.madeyski@gmail.com>
2025-05-26 14:49:13 +0200

    Legacy page

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,7 @@ import './style/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { i18n } from './i18n'
 
 const app = createApp(App)
-
-app.use(i18n)
 
 app.mount('#app')
